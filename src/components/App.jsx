bf0b3a378ac3d9fb81b2fe264ecf0e51f320a37e
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-11-09 19:32:05 +0200

    API imp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { Searchbar } from "./Searchbar/Searchbar";
-import { ImageGallery } from "./ImageGallery/ImageGallery";
-import { Button } from "./Button/Button";
-import { Modal } from "./Modal/Modal";
+import React from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Modal } from './Modal/Modal';
+// import { fetchImages } from "./services/services";
 // import css from './App.module.css'
 
 export const App = () => {
