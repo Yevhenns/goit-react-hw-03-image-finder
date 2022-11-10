d85065fb1acd9e05caec82f3fc282c61a4f32398
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-11-10 21:02:31 +0200

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
-// import { fetchImages } from "./services/services";
+import { fetchImages } from './services/services';
 // import css from './App.module.css'
 
 export class App extends Component {
@@ -11,14 +11,34 @@ export class App extends Component {
     isShown: false,
     images: [],
     page: 1,
+    isLoading: false,
+  };
+
+  showImages = () => {
+    this.setState(prevState => ({
+      isShown: !prevState.isShown,
+    }));
+  };
+
+  getImages = e => {
+    e.preventDefault();
+    this.setState({
+      isLoading: true,
+    });
+    fetchImages(this.state.page).then(resp => {
+      this.setState(prevState => ({
+        images: [...prevState.images, resp.data.hits],
+      }));
+      console.log(resp.data.hits);
+    });
   };
   render() {
     return (
       <div>
-        <Searchbar />
+        <Searchbar ClickHandler={this.getImages} />
         <ImageGallery />
         <Button />
-        <Modal />
+        {/* <Modal /> */}
       </div>
     );
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,20 +1,26 @@
-import React from "react";
-import css from './SearchBar.module.css'
+import React from 'react';
+import css from './SearchBar.module.css';
 
-export const Searchbar = () => {
-    return (<header className={css.Searchbar}>
-  <form className={css.SearchForm}>
-    <button type="submit" className={css.SearchFormButton}>
-      <span className={css.SearchFormButtonLabel}>Search</span>
-    </button>
+export const Searchbar = ({ text, ClickHandler }) => {
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm}>
+        <button
+          type="submit"
+          className={css.SearchFormButton}
+          onClick={ClickHandler}
+        >
+          <span className={css.SearchFormButtonLabel}>Search</span>
+        </button>
 
-    <input
-        className={css.SearchFormInput}
-        type="text"
-        autoComplete="off"
-        autoFocus
-        placeholder="Search images and photos"
-    />
-  </form>
-</header>)
-}
+        <input
+          className={css.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
